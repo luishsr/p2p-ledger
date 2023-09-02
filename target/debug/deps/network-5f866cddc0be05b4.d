7b9ffd244add7843d6028e4765b8adfc97eec006
luishsr <luishsoares@gmail.com>
2023-09-02 14:07:07 -0300

    Fixed package name

---
+++ target/debug/deps/network-5f866cddc0be05b4.d
@@ -0,0 +1,7 @@
+/home/luis/rust/p2p-ledger/target/debug/deps/network-5f866cddc0be05b4.rmeta: src/network.rs
+
+/home/luis/rust/p2p-ledger/target/debug/deps/libnetwork-5f866cddc0be05b4.rlib: src/network.rs
+
+/home/luis/rust/p2p-ledger/target/debug/deps/network-5f866cddc0be05b4.d: src/network.rs
+
+src/network.rs:
